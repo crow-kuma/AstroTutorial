d0eb28996c915f8c63822c6068ded529624ee990
gaugau71 <nishigauchi@tierline.com>
2025-07-19 03:00:04 +0900

    completed main tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://crows-inspiring-astro-puppy.netlify.app/",
+  integrations: [preact()]
+});
